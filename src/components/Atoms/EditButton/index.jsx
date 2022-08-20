a10eff8366cb351160b49d245f40fdb7925d9606
Tomu Fujikawa <ttomfjkw@gmail.com>
2022-08-20 10:18:32 +0900

    HoverCircleの定義をButtonより上でする

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -10,8 +10,8 @@ const EditButton = ({ onClick }) => {
       <img
         style={{
           position: "absolute",
-          top: 0,
-          left: 0,
+          top: 2.5,
+          left: 2.5,
           width: 15,
           height: 15,
         }}
@@ -22,21 +22,22 @@ const EditButton = ({ onClick }) => {
 };
 export default EditButton;
 
-const Button = styled.button`
-  background-color: transparent;
-  border-color: transparent;
-  position: relative;
-  cursor: pointer;
+const HoverCircle = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 20px;
   height: 20px;
+`;
+
+const Button = styled.button`
+  position: relative;
+  background-color: transparent;
+  border-color: transparent;
+  cursor: pointer;
   &:hover ${HoverCircle} {
     border-radius: 50%;
     background-color: ${COLOR.LIGHT_GRAY};
     opacity: 0.2;
   }
-`;
-const HoverCircle = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
 `;
