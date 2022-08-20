b80f725f537842543e7040e3f9b056f9004381fa
Tomu Fujikawa <ttomfjkw@gmail.com>
2022-08-20 14:27:52 +0900

    Atoms/AddTaskButtonの作成

---
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+import plus from "../../../assets/svg/plus.svg";
+import COLOR from "../../../variables/color";
+import TEXT from "../../../variables/texts";
+const AddTaskButton = (props) => {
+  return (
+    <Button onClick={props.onClick}>
+      <HoverEllipse />
+      <ImgLabelContainer>
+        <img
+          style={{
+            width: 20,
+            height: 20,
+            marginRight: 10,
+          }}
+          src={plus}
+        />
+        {props.label}
+      </ImgLabelContainer>
+    </Button>
+  );
+};
+export default AddTaskButton;
+
+const HoverEllipse = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 126px;
+  height: 24px;
+`;
+const ImgLabelContainer = styled.div`
+  position: absolute;
+  display: flex;
+  padding: 2px 6px;
+  top: 0;
+  left: 0;
+  color: ${COLOR.GREEN};
+  ${TEXT.S}
+  font-weight: 500;
+`;
+const Button = styled.button`
+  position: relative;
+  padding: 0;
+  width: 130px;
+  height: 24px;
+  background-color: transparent;
+  border-color: transparent;
+  cursor: pointer;
+  &:hover ${HoverEllipse} {
+    border-radius: 9999px;
+    background-color: ${COLOR.GREEN};
+    opacity: 0.2;
+  }
+`;
