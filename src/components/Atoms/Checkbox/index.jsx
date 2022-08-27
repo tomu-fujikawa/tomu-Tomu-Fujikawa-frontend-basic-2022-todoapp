d9eacfed80f310c9d769a855a2e22c9d431b3ede
Tomu Fujikawa <ttomfjkw@gmail.com>
2022-08-27 18:06:00 +0900

    masterからブランチを切ってCheckboxを作成する

---
+++ src/components/Atoms/Checkbox/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import check from "../../../assets/svg/check.svg";
+import COLOR from "../../../variables/color";
+
+const Checkbox = ({ onClick }) => {
+  return (
+    <Button onClick={onClick}>
+      <img src={check} />
+    </Button>
+  );
+};
+export default Checkbox;
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  width: 20px;
+  height: 20px;
+  border: 2px solid ${COLOR.LIGHT_GRAY};
+  border-radius: 2px;
+  cursor: pointer;
+  padding: 0;
+  & > img {
+    width: 14px;
+    display: none;
+  }
+  &:hover img {
+    display: block;
+  }
+`;
