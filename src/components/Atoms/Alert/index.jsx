25a6ba1c81c36adaf9ac91392962f2d4cae7805f
Tomu Fujikawa <ttomfjkw@gmail.com>
2023-01-08 21:22:41 +0900

    Alertのファイル作成

---
+++ src/components/Atoms/Alert/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import plus from "../../../assets/svg/plus.svg";
+import COLOR from "../../../variables/color";
+import TEXT from "../../../variables/texts";
+import FAMILY from "../../../variables/font_family";
+const Alert = () => {
+  return <StyledAlert></StyledAlert>;
+};
+export default Alert;
+const StyledAlert = styled.div``;
